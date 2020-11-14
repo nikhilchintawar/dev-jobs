bbe3c979d432a0fd232fb74640af2bd9ae9bdbfd
Nikhil Chintawar <chintawarnikhil88@gmail.com>
2020-11-14 11:15:22 +0530

    created the basic card sturcture for job listing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { ReactComponent as AccountLogo} from "./assets/account.svg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Dev Jobs</h1>
       </header>
+      <div className="job-card">
+          <AccountLogo />
+          <div className="company">
+            <h3 className="company-name">Account</h3>
+            <div className="job-freshness">NEW!</div>
+            <div className="job-featured">FEATURED</div>
+          </div>
+          <div className="job-role">Senior Frontend Developer</div>
+          <div className="job-info">
+            <span className="day-ago">1d ago</span>
+            <span className="job-type">Full Time</span>
+            <span className="job-location">INDIA only</span>
+          </div>
+          <div className="job-skills">
+            <span>Frontend</span>
+            <span>Senior</span>
+            <span>HTML</span>
+            <span>CSS</span>
+            <span>JavaScript</span>
+          </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
